0147c41fd171c7570db79f88bbd274926ed82cbc
Tsubasa Ishihara <tishihar@c6r2s8.42tokyo.jp>
2026-04-08 01:48:47 +0900

    change mariadb_setup

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -9,16 +9,30 @@ chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-    mysqld --user=mysql --bootstrap << EOF
-USE mysql;
-FLUSH PRIVILEGES;
+	mysqld --user=mysql --skip-networking --socket=/run/mysqld/mysqld.sock &
+	pid="$!"
+
+	until mariadb-admin --socket=/run/mysqld/mysqld.sock ping >/dev/null 2>&1; do
+		sleep 1
+	done
+
+	mariadb --socket=/run/mysqld/mysqld.sock << EOF
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
+
+	kill "$pid"
+	wait "$pid"
 fi
 
 exec mysqld --user=mysql --console
